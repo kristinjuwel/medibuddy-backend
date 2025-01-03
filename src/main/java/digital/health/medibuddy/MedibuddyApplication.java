f729cdf61804068cea50154a4056d7d9535d8e54
Jewel Malimban <krisdiajewls@gmail.com>
2025-01-03 22:03:47 +0800

    fix: typo

--- src/main/java/digital/health/medibuddy/MedibuddyApplication.java
+++ src/main/java/digital/health/medibuddy/MedibuddyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MedibuddyApplicataddion {
+public class MedibuddyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedibuddyApplication.class, args);
